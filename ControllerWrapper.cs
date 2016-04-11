ccd69522944cee98004a99e874a8485882c5e463
BeyondReal <trevor@beyondreality.ca>
2016-04-11 00:35:56 -0600

    Added files via upload

---
+++ ControllerWrapper.cs
@@ -0,0 +1,192 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR;
+/// <summary>
+/// This script must be on a controller to function
+/// 
+/// The purpose of this is to send events using the SteamVR.Event system. 
+/// When using this script game objects do not need a reference to the controllers, they only need 
+/// to listen for the event. (SteamVR.Event.Listen("controllerEvent", OnControllerEvent)
+/// 
+/// The listening object will need to recast the (params ojbect[] args) to ControllerEventArgs
+/// ControllerEventArgs eventStruct = (ControllerEventArgs)args[0];
+/// And the parameters can be accessed from there. 
+/// 
+/// Credit: This script structure as taken from "SteamVR_TrackedController" in the Steam Unity VR package
+/// And was made working with the help from Lee Wasilenko at www.vrdevschool.com
+///  
+/// Extensions: This class can be extended to get the controller rotation and axis information. 
+/// The hair trigger method can be added as well 
+/// 
+/// Notes: The SteamVR event system uses the following pattern to check for null. 
+/// The EtrackingResults are stored in the Valve name space. 
+/// If this script needs to check for events then what are they? 
+/// 
+/// static public bool initializing { get; private set; }
+///
+/// var initializing = result == ETrackingResult.Uninitialized;
+/// if (initializing != SteamVR.initializing)
+/// {
+///	SteamVR_Utils.Event.Send("initializing", initializing);
+/// }
+/// </summary>
+/// 
+
+
+
+
+//All parameters here will be passed on the event trigger
+public struct ControllerEventArgs {
+    public uint controllerIndex;
+    public uint flags;
+    public float padX, padY;
+}
+
+
+public class ControllerWrapper : MonoBehaviour {
+
+
+    public uint controllerIndex;
+    public VRControllerState_t controllerState;
+    public bool triggerPressed = false;
+    public bool steamPressed = false;
+    public bool menuPressed = false;
+    public bool padPressed = false;
+    public bool padTouched = false;
+    public bool gripped = false;
+
+
+   
+
+
+// Use this for initialization
+void Start() {
+
+        //Get a reference to the tracked object class 
+        if (this.GetComponent<SteamVR_TrackedObject>() == null) {
+            gameObject.AddComponent<SteamVR_TrackedObject>();
+        }
+
+        //Get the index this controller is
+        controllerIndex = (uint)this.GetComponent<SteamVR_TrackedObject>().index;
+        if (this.GetComponent<SteamVR_RenderModel>() != null) {
+            this.GetComponent<SteamVR_RenderModel>().index = (SteamVR_TrackedObject.EIndex)controllerIndex;
+        }
+    }
+
+
+   
+
+   
+    // Update is called once per frame
+    void Update() {
+
+        var system = OpenVR.System;
+        if (system != null && system.GetControllerState(controllerIndex, ref controllerState)) {
+
+            ulong trigger = controllerState.ulButtonPressed & (1UL << ((int)EVRButtonId.k_EButton_SteamVR_Trigger));
+            if (trigger > 0L && !triggerPressed) {
+                triggerPressed = true;
+                ControllerEventArgs e;
+                e.controllerIndex = controllerIndex;
+                e.flags = (uint)controllerState.ulButtonPressed;
+                e.padX = controllerState.rAxis0.x;
+                e.padY = controllerState.rAxis0.y;
+                //I beleive the null check is handled in the SteamVR_Utils before the event is sent
+                //TODO see if this good enough null checking
+                SteamVR_Utils.Event.Send("triggerClicked", e);
+
+            } else if (trigger == 0L && triggerPressed) {
+                triggerPressed = false;
+                ControllerEventArgs e;
+                e.controllerIndex = controllerIndex;
+                e.flags = (uint)controllerState.ulButtonPressed;
+                e.padX = controllerState.rAxis0.x;
+                e.padY = controllerState.rAxis0.y;
+                SteamVR_Utils.Event.Send("triggerUnclicked", e);
+               
+            }
+
+            ulong grip = controllerState.ulButtonPressed & (1UL << ((int)EVRButtonId.k_EButton_Grip));
+            if (grip > 0L && !gripped) {
+                gripped = true;
+                ControllerEventArgs e;
+                e.controllerIndex = controllerIndex;
+                e.flags = (uint)controllerState.ulButtonPressed;
+                e.padX = controllerState.rAxis0.x;
+                e.padY = controllerState.rAxis0.y;
+                SteamVR_Utils.Event.Send("gripClicked", e);
+
+            } else if (grip == 0L && gripped) {
+                gripped = false;
+                ControllerEventArgs e;
+                e.controllerIndex = controllerIndex;
+                e.flags = (uint)controllerState.ulButtonPressed;
+                e.padX = controllerState.rAxis0.x;
+                e.padY = controllerState.rAxis0.y;
+                SteamVR_Utils.Event.Send("gripUnclicked", e);
+            }
+
+            ulong pad = controllerState.ulButtonPressed & (1UL << ((int)EVRButtonId.k_EButton_SteamVR_Touchpad));
+            if (pad > 0L && !padPressed) {
+                padPressed = true;
+                ControllerEventArgs e;
+                e.controllerIndex = controllerIndex;
+                e.flags = (uint)controllerState.ulButtonPressed;
+                e.padX = controllerState.rAxis0.x;
+                e.padY = controllerState.rAxis0.y;
+                SteamVR_Utils.Event.Send("padClicked", e);
+
+            } else if (pad == 0L && padPressed) {
+                padPressed = false;
+                ControllerEventArgs e;
+                e.controllerIndex = controllerIndex;
+                e.flags = (uint)controllerState.ulButtonPressed;
+                e.padX = controllerState.rAxis0.x;
+                e.padY = controllerState.rAxis0.y;
+                SteamVR_Utils.Event.Send("padUnclicked", e);
+               
+            }
+
+            ulong menu = controllerState.ulButtonPressed & (1UL << ((int)EVRButtonId.k_EButton_ApplicationMenu));
+            if (menu > 0L && !menuPressed) {
+                menuPressed = true;
+                ControllerEventArgs e;
+                e.controllerIndex = controllerIndex;
+                e.flags = (uint)controllerState.ulButtonPressed;
+                e.padX = controllerState.rAxis0.x;
+                e.padY = controllerState.rAxis0.y;
+                SteamVR_Utils.Event.Send("menuClicked", e);
+
+            } else if (menu == 0L && menuPressed) {
+                menuPressed = false;
+                ControllerEventArgs e;
+                e.controllerIndex = controllerIndex;
+                e.flags = (uint)controllerState.ulButtonPressed;
+                e.padX = controllerState.rAxis0.x;
+                e.padY = controllerState.rAxis0.y;
+                SteamVR_Utils.Event.Send("menuUnclicked", e);
+            }
+
+            pad = controllerState.ulButtonTouched & (1UL << ((int)EVRButtonId.k_EButton_SteamVR_Touchpad));
+            if (pad > 0L && !padTouched) {
+                padTouched = true;
+                ControllerEventArgs e;
+                e.controllerIndex = controllerIndex;
+                e.flags = (uint)controllerState.ulButtonPressed;
+                e.padX = controllerState.rAxis0.x;
+                e.padY = controllerState.rAxis0.y;
+                SteamVR_Utils.Event.Send("padTouched", e);
+
+            } else if (pad == 0L && padTouched) {
+                padTouched = false;
+                ControllerEventArgs e;
+                e.controllerIndex = controllerIndex;
+                e.flags = (uint)controllerState.ulButtonPressed;
+                e.padX = controllerState.rAxis0.x;
+                e.padY = controllerState.rAxis0.y;
+                SteamVR_Utils.Event.Send("padUntouched", e);
+            }
+        }
+    }
+}
---
+++ FindController.cs
@@ -0,0 +1,214 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+///
+/// This class works wiht the "ControllerWrapper" class and is used to receive events from it. 
+/// To use this class place it on any game object that should receive events from a controller (i.e. - button press) 
+/// Modify as nessacary. 
+/// 
+/// This class is far from effecient and may have errors. So you have optimizations or find errors please let me know 
+/// or better yet provide the solutions. 
+/// 
+/// </summary>
+
+public class FindController : MonoBehaviour {
+
+    //Left controller index reference 
+    uint lt = 0;
+    uint rt = 0;
+
+    public uint controllerIndex;
+    public bool triggerPressed = false;
+    public bool steamPressed = false;
+    public bool menuPressed = false;
+    public bool padPressed = false;
+    public bool padTouched = false;
+    public bool gripped = false;
+
+
+    // private SteamVR_Controller.Device rtController { get { return SteamVR_Controller.Input((int)rt); } }
+    private SteamVR_Controller.Device rtController;
+
+
+    public void OnEnable() {
+        SteamVR_Utils.Event.Listen("device_connected", OnDeviceConnected);
+
+        SteamVR_Utils.Event.Listen("triggerClicked", OnTriggerClicked);
+        SteamVR_Utils.Event.Listen("triggerUnclicked", OnTriggerUnclicked);
+        SteamVR_Utils.Event.Listen("gripClicked", OnGripClicked);
+        SteamVR_Utils.Event.Listen("gripUnclicked", OnGripUnclicked);
+        SteamVR_Utils.Event.Listen("padClicked", OnPadClicked);
+        SteamVR_Utils.Event.Listen("padUnlicked", OnPadUnclicked);
+        SteamVR_Utils.Event.Listen("menuClicked", OnMenulicked);
+        SteamVR_Utils.Event.Listen("menuUnclicked", OnMenuUnclicked);
+        SteamVR_Utils.Event.Listen("padTouched", OnPadTouched);
+        SteamVR_Utils.Event.Listen("padUntouched", OnPadUntouched);
+
+
+
+    }
+    public void OnDisable() {
+        SteamVR_Utils.Event.Remove("device_connected", OnDeviceConnected);
+
+        SteamVR_Utils.Event.Remove("triggerClicked", OnTriggerClicked);
+        SteamVR_Utils.Event.Remove("triggerUnclicked", OnTriggerUnclicked);
+        SteamVR_Utils.Event.Remove("gripClicked", OnGripClicked);
+        SteamVR_Utils.Event.Remove("gripUnclicked", OnGripUnclicked);
+        SteamVR_Utils.Event.Remove("padClicked", OnPadClicked);
+        SteamVR_Utils.Event.Remove("padUnlicked", OnPadUnclicked);
+        SteamVR_Utils.Event.Remove("menuClicked", OnMenulicked);
+        SteamVR_Utils.Event.Remove("menuUnclicked", OnMenuUnclicked);
+        SteamVR_Utils.Event.Remove("padTouched", OnPadTouched);
+        SteamVR_Utils.Event.Remove("padUntouched", OnPadUntouched);
+    }
+
+
+    private void OnTriggerClicked(params object[] args) {
+       
+        ControllerEventArgs eventStruct = (ControllerEventArgs)args[0];
+
+        Debug.Log("OnTriggerClicked");
+        Debug.Log("Controller Index is " + eventStruct.controllerIndex);
+        Debug.Log("The flags are " + eventStruct.flags);
+        Debug.Log("Touchpad X is " + eventStruct.padX);
+        Debug.Log("Touchpad Y is  " + eventStruct.padY);
+
+        DebugTest(eventStruct.controllerIndex);
+
+    }
+
+    private void OnTriggerUnclicked(params object[] args) {
+      
+        ControllerEventArgs eventStruct = (ControllerEventArgs)args[0];
+
+        Debug.Log("OnTriggerUnclicked");
+        Debug.Log("Controller Index is " + eventStruct.controllerIndex);
+        Debug.Log("The flags are " + eventStruct.flags);
+        Debug.Log("Touchpad X is " + eventStruct.padX);
+        Debug.Log("Touchpad Y is  " + eventStruct.padY);
+    }
+
+
+    private void OnGripClicked(params object[] args) {
+       
+        ControllerEventArgs eventStruct = (ControllerEventArgs)args[0];
+
+        Debug.Log("OnGripClicked");
+        Debug.Log("Controller Index is " + eventStruct.controllerIndex);
+        Debug.Log("The flags are " + eventStruct.flags);
+        Debug.Log("Touchpad X is " + eventStruct.padX);
+        Debug.Log("Touchpad Y is  " + eventStruct.padY);
+    }
+
+    private void OnGripUnclicked(params object[] args) {
+
+        ControllerEventArgs eventStruct = (ControllerEventArgs)args[0];
+
+        Debug.Log("OnGripUnclicked");
+        Debug.Log("Controller Index is " + eventStruct.controllerIndex);
+        Debug.Log("The flags are " + eventStruct.flags);
+        Debug.Log("Touchpad X is " + eventStruct.padX);
+        Debug.Log("Touchpad Y is  " + eventStruct.padY);
+    }
+
+    private void OnPadClicked(params object[] args) {
+
+        ControllerEventArgs eventStruct = (ControllerEventArgs)args[0];
+
+        Debug.Log("OnPadClicked");
+        Debug.Log("Controller Index is " + eventStruct.controllerIndex);
+        Debug.Log("The flags are " + eventStruct.flags);
+        Debug.Log("Touchpad X is " + eventStruct.padX);
+        Debug.Log("Touchpad Y is  " + eventStruct.padY);
+    }
+
+    private void OnPadUnclicked(params object[] args) {
+
+        ControllerEventArgs eventStruct = (ControllerEventArgs)args[0];
+
+        Debug.Log("OnPadUnclicked");
+        Debug.Log("Controller Index is " + eventStruct.controllerIndex);
+        Debug.Log("The flags are " + eventStruct.flags);
+        Debug.Log("Touchpad X is " + eventStruct.padX);
+        Debug.Log("Touchpad Y is  " + eventStruct.padY);
+    }
+
+    private void OnMenulicked(params object[] args) {
+
+        ControllerEventArgs eventStruct = (ControllerEventArgs)args[0];
+
+        Debug.Log("OnMenulicked");
+        Debug.Log("Controller Index is " + eventStruct.controllerIndex);
+        Debug.Log("The flags are " + eventStruct.flags);
+        Debug.Log("Touchpad X is " + eventStruct.padX);
+        Debug.Log("Touchpad Y is  " + eventStruct.padY);
+    }
+
+    private void OnMenuUnclicked(params object[] args) {
+
+        ControllerEventArgs eventStruct = (ControllerEventArgs)args[0];
+
+        Debug.Log("OnMenuUnclicked");
+        Debug.Log("Controller Index is " + eventStruct.controllerIndex);
+        Debug.Log("The flags are " + eventStruct.flags);
+        Debug.Log("Touchpad X is " + eventStruct.padX);
+        Debug.Log("Touchpad Y is  " + eventStruct.padY);
+    }
+
+    private void OnPadTouched(params object[] args) {
+
+        ControllerEventArgs eventStruct = (ControllerEventArgs)args[0];
+
+        Debug.Log("OnPadTouched");
+        Debug.Log("Controller Index is " + eventStruct.controllerIndex);
+        Debug.Log("The flags are " + eventStruct.flags);
+        Debug.Log("Touchpad X is " + eventStruct.padX);
+        Debug.Log("Touchpad Y is  " + eventStruct.padY);
+    }
+
+    private void OnPadUntouched(params object[] args) {
+
+        ControllerEventArgs eventStruct = (ControllerEventArgs)args[0];
+
+        Debug.Log("OnPadUntouched");
+        Debug.Log("Controller Index is " + eventStruct.controllerIndex);
+        Debug.Log("The flags are " + eventStruct.flags);
+        Debug.Log("Touchpad X is " + eventStruct.padX);
+        Debug.Log("Touchpad Y is  " + eventStruct.padY);
+    }
+
+    //This gets the index for the contollers. 
+    //TODO get index based on "right/left most" to get accurate results
+    private void OnDeviceConnected(params object[] args) {
+
+        if ((int) args[0] != 0) {
+        var i = (int)args[0]; 
+        bool connected = (bool)args[1]; 
+        var vr = SteamVR.instance;
+        if (connected) {
+            if (vr.hmd.GetTrackedDeviceClass((uint)i) == Valve.VR.ETrackedDeviceClass.Controller) {
+                if (rt == 0) {
+                    rt = (uint)i;
+                    Debug.Log("Right Controller index is " + rt);
+                }else if (lt == 0) {
+                    lt = (uint)i;
+                    Debug.Log("Left controller index is " + lt);
+                }
+            }
+          }
+        }
+    }
+
+
+
+   void DebugTest(uint controllerIndex) {
+        //Simple usage 
+            if (controllerIndex == rt) {
+                Debug.Log("Right Trigger pulled"); 
+            } else {
+            Debug.Log("Left Trigger pulled");
+        }
+        
+    }
+}
